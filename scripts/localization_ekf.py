27210de089b911c1a19828be63d113f7428d078b
Elerson Santos <elerson@gmail.com>
2021-10-07 16:16:20 -0300

    adding confidence to initialization

--- scripts/localization_ekf.py
+++ scripts/localization_ekf.py
@@ -23,9 +23,9 @@ class LocalizationEKF:
         self.odom_pub = rospy.Publisher('localization_odom_ekf', Odometry, queue_size=10)
 
         #self.current_pose = np.matrix([[0],[0],[0]])
-        self.sigma  = np.matrix(np.identity((3)))*100000.0
+        self.sigma  = np.matrix(np.identity((3)))*0.0001
         self.current_odom_time = time.time()
-        self.current_odom = np.matrix([[0.001],[0.001],[0.001]])
+        self.current_odom = np.matrix([[0],[0],[0]])
 
         self.setInitialPose(initial_pose)
 
